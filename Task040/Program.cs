a4522da582a79ced4857691b97aa17308284c578
Dmitry <dmitrbah@mail.ru>
2023-10-10 17:11:43 +0300

    Added Task 040

---
+++ Task040/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число: ");
+int firstLength = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int secondLength = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int thirdLength = Convert.ToInt32(Console.ReadLine());
+
+bool IsExistTriangle(int a, int b, int c)
+{
+    return (a + b) >= c && (b + c) >= a && (c + a) >= b;
+}
+
+bool result = IsExistTriangle(firstLength, secondLength, thirdLength);
+Console.WriteLine(result ? "Треугольник существует" : "Треугольник не существует");
