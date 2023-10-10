6da166ef0cb8dae06bd3af4dcf492c14355c9bfa
Dmitry <dmitrbah@mail.ru>
2023-10-10 19:11:51 +0300

    Added Task 041 Homework

---
+++ Task041/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите размер массива: ");
+int arraySize = Convert.ToInt32(Console.ReadLine());
+
+int[] UserArray(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int PositiveNumberCounts(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) count += 1;
+    }
+    return count;
+}
+
+int[] array = UserArray(arraySize);
+PrintArray(array);
+int result = PositiveNumberCounts(array);
+Console.WriteLine(result);
