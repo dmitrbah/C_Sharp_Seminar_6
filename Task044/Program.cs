ffa6d1c8da0af7e55c80c45270356c385849280a
Dmitry <dmitrbah@mail.ru>
2023-10-10 18:09:20 +0300

    Added Task 044

---
+++ Task044/Program.cs
@@ -0,0 +1,29 @@
+﻿int[] Fibonachi(int num)
+{
+    int[] fibonachi = new int[num];
+    // fibonachi[0] = 0;
+    fibonachi[1] = 1;
+    for (int i = 2; i < fibonachi.Length; i++)
+    {
+        fibonachi[i] = fibonachi[i-1] + fibonachi[i-2];
+    }
+    return fibonachi;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] result = Fibonachi(number);
+PrintArray(result);
+
