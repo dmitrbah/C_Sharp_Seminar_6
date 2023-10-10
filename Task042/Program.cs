69b4af232d3cc879a4dd8c81651009a815154bff
Dmitry <dmitrbah@mail.ru>
2023-10-10 17:51:40 +0300

    Added Task 042

---
+++ Task042/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите основание системы счисления: ");
+int baseNumber = Convert.ToInt32(Console.ReadLine());
+
+int DecToBaseNum(int number, int baseNum)
+{
+    int result = 0;
+    int mult = 1;
+    while (number > 0)
+    {
+        result += number % baseNum * mult;
+        number = number / 2;
+        mult *= 10;
+    }
+    return result;
+}
+
+int res = DecToBaseNum(number, baseNumber);
+Console.WriteLine(res);
